cd2736871f540311b6f7166b097c24508c267bae
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2023-03-06 15:22:02 +0800

    remove export

--- xmake.lua
+++ xmake.lua
@@ -2,8 +2,6 @@ _config_project({
 	project_name = "lc-unity3d",
 	project_kind = "shared"
 })
-local add_includedirs = _get_add_includedirs()
-local add_defines = _get_add_defines()
 add_deps("lc-runtime")
 add_files("**.cpp")
 add_syslinks("DXGI")
